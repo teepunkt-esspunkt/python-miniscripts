223468c06226c79329ed6242796e1b65b5ef20ed
Tarek Saleh <tareks-mail@proton.me>
2026-01-15 13:56:53 +0100

    Change file move to copy and filter by .mkv
    Updated the script to copy files instead of moving them and added a filter to only process .mkv files.

--- move_files_out_of_subfolder.py
+++ move_files_out_of_subfolder.py
@@ -2,9 +2,6 @@
 import os
 import shutil
 
-# Absolute path to the current script
-#script_path = os.path.abspath(__file__)
-
 # Directory where the script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,6 +10,8 @@ for root, dirs, files in os.walk(script_dir):
         continue
 
     for file in files:
+        if not file.lower().endswith(".mkv"): #only *.mkv   comment out if all files wanted
+            continue #comment out for all files
         file_path = os.path.join(root, file)
         destination = os.path.join(script_dir, file)
 
@@ -23,6 +22,7 @@ for root, dirs, files in os.walk(script_dir):
                 destination = os.path.join(script_dir, f"{name}_{counter}{ext}")
                 counter += 1
 
-        shutil.move(file_path, destination)
+        shutil.copy2(file_path, destination)
+        #shutil.move(file_path, destination) #move
 
 print("done:", script_dir)
